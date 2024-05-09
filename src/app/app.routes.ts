228be0e62f5f89dcdc84aa946c72649c3b793031
Pablo <pablocamacho92@gmail.com>
2024-05-09 17:43:54 -0300

    Creo la estructura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './auth/layout/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { DashboardLayoutComponent } from './dashboard/layout/dashboard-layout/dashboard-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./auth/layout/auth-layout/auth-layout.component').then(
+        (c) => c.AuthLayoutComponent
+      ),
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./auth/pages/login-page/login-page.component').then(
+            (c) => c.LoginPageComponent
+          ),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./auth/pages/register-page/register-page.component').then(
+            (c) => c.RegisterPageComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import(
+        './dashboard/layout/dashboard-layout/dashboard-layout.component'
+      ).then((c) => c.DashboardLayoutComponent),
+  },
+];
