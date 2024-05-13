f77affcd604021b0190730c42384dc9c95dbfe00
Pablo <pablocamacho92@gmail.com>
2024-05-13 11:44:19 -0300

    Recupero Borrado

--- src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -2,11 +2,31 @@ import { Component, computed, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth-service.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupNewPlantComponent } from '../popup-new-plant/popup-new-plant.component';
+import { MatButtonModule } from '@angular/material/button';
+import { PopupEditPlantComponent } from '../popup-edit-plant/popup-edit-plant.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTableModule,
+    MatListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
 })
@@ -14,6 +34,33 @@ export class DashboardLayoutComponent {
   private authService = inject(AuthService);
   private router = inject(Router);
   public user = computed(() => this.authService.currentUser());
+
+  constructor(private dialog: MatDialog) {}
+
+  createPlant() {
+    const dialogRef = this.dialog.open(PopupNewPlantComponent, {
+      maxWidth: '400px',
+      width: '400px',
+      height: 'auto',
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(result);
+    });
+  }
+
+  editPlant(plant: any) {
+    const dialogRef = this.dialog.open(PopupEditPlantComponent, {
+      maxWidth: '600%',
+      width: '600px',
+      height: 'auto',
+      data: plant,
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      console.log(result);
+    });
+  }
 
   onLogout() {
     this.authService.logout();
